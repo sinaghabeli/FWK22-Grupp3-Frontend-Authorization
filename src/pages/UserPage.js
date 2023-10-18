775f1162fbcdcd0d969655cf83d4b4520c1a7471
AD1121 <sinagh30@gmail.com>
2023-10-18 21:05:53 +0200

    #9 Logout fixed and done

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -1,9 +1,42 @@
-import React from "react";
+import "./userpage.css";
+import { useNavigate } from "react-router-dom";
 
 function UserPage() {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("accessToken");
+    navigate(`/`);
+  };
+
+  const fetchData = async () => {
+    try {
+      const token = localStorage.getItem("access_token");
+      const response = await fetch("data", {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+
+      if (!response.ok) {
+        if (response.status === 401) {
+          handleLogout();
+          alert("Session expired. Please login again.");
+          return;
+        }
+        throw new Error("Failed to fetch data from backend");
+      }
+
+      const result = await response.json();
+      setData(result.data);
+    } catch (error) {
+      alert(error.message || "Failed to fetch data");
+    }
+  };
+
   return (
     <div>
       <h1>User Page</h1>
+      <button onClick={fetchData}>Fetch Data</button>
+      <button onClick={handleLogout}>Logout</button>
     </div>
   );
 }
